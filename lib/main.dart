fb1c22fd15c466b2b87d8d4d5928c7a4dbb04345
An Van Nguyen <152575252+anvng@users.noreply.github.com>
2024-10-06 23:22:53 +0700

    refactor: move widgets folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tanjun_app/modules/views/home_screen.dart';
+import 'package:tanjun_app/modules/views/task_screen.dart';
 
 void main() {
   runApp(const TanjunApp());
@@ -15,7 +15,7 @@ class TanjunApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const HomeScreen(),
+        home: const TaskScreen(),
         debugShowCheckedModeBanner: false);
   }
 }
---
+++ lib/widgets/task_bar_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TaskBar extends StatelessWidget {
+  const TaskBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
